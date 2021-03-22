0aa25d7f77a2c20f9de0bb87d516943d62f79d57
jmigu <jmigu@JoseMiguel>
2021-03-22 11:29:42 +0100

    Segundo commit. Después de refactorizar.

--- src/daw_ed04_actividad/Main.java
+++ src/daw_ed04_actividad/Main.java
@@ -9,21 +9,28 @@ package daw_ed04_actividad;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta miCuenta;
+        CCuenta cuenta1;
         double saldoActual;
+        String nombreCuenta;
+        
+        cuenta1 = new CCuenta("Juan Gutiérrez","1300-2665-85-4433221144",2500,0);
+        saldoActual = cuenta1.estado();
+        nombreCuenta = cuenta1.getNombre();
+                
+        System.out.println("El saldo actual " + nombreCuenta + " es "+ saldoActual );
 
-        miCuenta = new CCuenta("Juan Gutiérrez","1300-2665-85-4433221144",2500,0);
-        saldoActual = miCuenta.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        opera(cuenta1, 250, 1000);
+    }
 
+    public static void opera(CCuenta cuenta1, float cantRetirar, float cantIngresar) {
         try {
-            miCuenta.retirar(2300);
+            cuenta1.retirar(cantRetirar);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
         try {
             System.out.println("Ingreso en cuenta");
-            miCuenta.ingresar(695);
+            cuenta1.ingresar(cantIngresar);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
